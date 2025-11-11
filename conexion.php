584a6ca14171ad894862bd5e719b477b335fb586
FranCRD <francocordoba07@gmail.com>
2025-11-11 17:56:52 -0600

    Actualizacion conexion.php
    Archivo de conexión corregido.

--- conexion.php
+++ conexion.php
@@ -1,8 +1,9 @@
 <?php
 
-$conexion = mysqli_connect("localhost", "root", "", "tsur_bd");
+$conexion = mysqli_connect("b9hprrabbejjr3xtqxxm-mysql.services.clever-cloud.com", "uwopynuxpa6zzxtm", "19C7VemG9CwI70I6sQis", "b9hprrabbejjr3xtqxxm");
 if (mysqli_connect_errno()) {
     printf("Fallo la conexion: %s\n", mysqli_connect_error());
 } 
+
 
 ?>
