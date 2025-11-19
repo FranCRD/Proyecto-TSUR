d296a70aa6c23633c0b5f05e9290bbbffe722573
Juan Daniel <jimenzg78@gmail.com>
2025-11-18 19:13:31 -0600

    Mejora seguridad en la validacion LOGIN
    Se actualizo que al hacer login para evitar inyecciones SQL se hace la consulta mediante una sentencia fija por el nombre de usuario, y luego se hace la verificacion de la contraseña encriptada en la base de datos.

--- validacionLOGIN.php
+++ validacionLOGIN.php
@@ -2,23 +2,34 @@
 session_start();
 include("conexion.php");
 
-$mensaje_error = ""; // Variable para almacenar el mensaje de error
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST['nombre_usuario'];
     $contrasena = $_POST['contrasena'];
 
-    $sql = "SELECT * FROM usuarios WHERE nombre_usuario = '$nombre' AND password = '$contrasena'";
-    $resultado = $conexion->query($sql);
+    //Consulta solo por nombre usando sentencia preparada para evitar inyecciones SQL
+    $sql = $conexion->prepare("SELECT * FROM usuarios WHERE nombre_usuario = ?");
+    $sql->bind_param("s", $nombre);
+    $sql->execute();
+    $resultado = $sql->get_result();
 
     if ($resultado->num_rows > 0) {
+
+        $usuario = $resultado->fetch_assoc();
+        //Verificar la contraseña encriptada
+        if (password_verify($contrasena, $usuario['password'])) {
         $_SESSION['nombre_usuario'] = $nombre;
+
+        //Si es admin
+        if (isset($usuario['es_admin'])) {
+            $_SESSION['es_admin'] = $usuario['es_admin'];
+        }
+
         header("Location: index.php");
         exit();
-    } else {
-        // Si los datos no coinciden, guardamos un mensaje genérico
-        header("Location: login.php?error=1");
-
-    }
+     }
 } 
+    // Si no coincide el usuario o la contraseña
+    header("Location: login.php?error=1");
+    exit();
+}
 ?>
